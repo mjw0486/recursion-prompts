e3c176af61fe346af46f2239885727e80c5b3d5c
Michael Wetterauer <mjw0486@gmail.com>
2020-08-31 16:30:25 -0400

    modulo: Lots of conditions...

--- src/recursion.js
+++ src/recursion.js
@@ -149,6 +149,21 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (y === 0) {
+    return "NaN";
+  } else if (x === 0) {
+    return 0;
+  } else if (y > x && x > 0 || y < x && x < 0 || y < x - x - x && x > 0 || y > x - x - x && x < 0) {
+    return x;
+  } else if (y === x  || x === y - y - y && x > 0) {
+    return 0;
+  } else if (y === x || x === y - y - y && x < 0) {
+    return -0;
+  } else if (x > 0 && y > 0 || x < 0 && y < 0) {
+    return modulo(x - y, y);
+  } else if (x > 0 && y < 0 || x < 0 && y > 0) {
+    return modulo(x + y, y)
+  }
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
