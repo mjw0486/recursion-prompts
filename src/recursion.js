c1d2ae18d5a0cb968989f6e8cfbb7c68bfedee08
Michael Wetterauer <mjw0486@gmail.com>
2020-08-31 12:46:00 -0400

    reverse: Edge Case: string.length=0: return ''. Base Case: string.length=1: return string. Recursion: return string[string.length-1] + reverse(string.slice(0,string.length-1));

--- src/recursion.js
+++ src/recursion.js
@@ -119,6 +119,13 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 1) {
+    return string;
+  } else if (string.length === 0) {
+    return '';
+  } else {
+    return string[string.length-1] + reverse(string.slice(0,string.length-1));
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
