782f47ef40e5b140d06f95e69b9dca9e7b39f845
Michael Wetterauer <mjw0486@gmail.com>
2020-08-31 11:30:17 -0400

    range: Base Case: If the difference between x and y is -1 <= diff <= 1, return []. Recursion (x < y): return [x+1].concat(range(x+1); Recursion (x > y): return [x-1].concat(range(x-1);

--- src/recursion.js
+++ src/recursion.js
@@ -71,6 +71,13 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x - y >= -1 && x - y <= 1) {
+    return [];
+  } else if (x < y) {
+    return [x + 1].concat(range(x + 1, y))
+  } else if (x > y) {
+    return [x - 1].concat(range(x - 1, y))
+  }
 };
 
 // 7. Compute the exponent of a number.
