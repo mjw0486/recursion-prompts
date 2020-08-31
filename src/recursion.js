27358a10d35f4e13e35d1902a42588f94658df7f
Michael Wetterauer <mjw0486@gmail.com>
2020-08-31 13:07:37 -0400

    palindrome: Edge Case: string.length = 0: return true; Base Case: string.length=1: return true; Base Case: string.length=2 and both letters are equal(after calling .toLowerCase() on them: return true, otherwise return false. Recursion: return palindrome(string.slice(1, string.length-1));

--- src/recursion.js
+++ src/recursion.js
@@ -130,6 +130,17 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length === 0) {
+    return true;
+  } else if (string.length === 1) {
+    return true;
+  } else if (string.length === 2 && string[0].toLowerCase() === string[1].toLowerCase()) {
+    return true;
+  } else if (string[0].toLowerCase() !== string[string.length-1].toLowerCase()) {
+    return false;
+  } else {
+    return palindrome(string.slice(1, string.length-1));
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
