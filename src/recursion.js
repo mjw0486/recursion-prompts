f1907be38c193a1f31df43cda3973218caa0d99d
Michael Wetterauer <mjw0486@gmail.com>
2020-08-31 16:50:05 -0400

    multiply: Base Case: if x or y = 0, return 0. Recursion (both pos): return x + multiply(x, y - 1). Recursion (both neg): return (x - x - x) + multiply(x, y + 1). Recursion (one neg): return (the negative argument) + multiply((the neg arg), (the pos arg) - 1)

--- src/recursion.js
+++ src/recursion.js
@@ -169,6 +169,17 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  } else if (x > 0 && y > 0) {
+    return x + multiply(x, y - 1);
+  } else if (x > 0 && y < 0) {
+    return y + multiply(y, x - 1);
+  } else if (x < 0 && y > 0) {
+    return x + multiply(x, y - 1);
+  } else if (x < 0 && y < 0) {
+    return (x - x - x) + multiply(x, y + 1);
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
