752e15e12643dfe835dc9dfbdbc34460d0dc55b5
Michael Wetterauer <mjw0486@gmail.com>
2020-09-01 15:55:54 -0400

    countValuesInObj:

--- src/recursion.js
+++ src/recursion.js
@@ -351,6 +351,18 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  let result = 0;
+
+  for (let property in obj) {
+    if (obj[property] === value) {
+      result ++;
+    }
+    if (typeof obj[property] === 'object') {
+      result += countValuesInObj(obj[property], value);
+    }
+  }
+
+  return result;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
