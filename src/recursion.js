d6d89dff098d649a964ac66e421894e184db1137
Michael Wetterauer <mjw0486@gmail.com>
2020-09-01 20:28:19 -0400

    compress: Base Case: list.length <=1, return list. Recursion: if list[0] != list[1], return [list[0]].concat(compress(list.slice(1))); Recursion: else, return compress(list.slice(1));

--- src/recursion.js
+++ src/recursion.js
@@ -508,6 +508,15 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+
+  if (list.length <= 1) {
+    return list;
+  } else if (list[0] !== list[1]) {
+    return [list[0]].concat(compress(list.slice(1)));
+  } else {
+    return compress(list.slice(1));
+  }
+
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
