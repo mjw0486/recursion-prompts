c43ce29537883e9677107a705c2e97f0aec27861
Michael Wetterauer <mjw0486@gmail.com>
2020-09-01 21:17:20 -0400

    alternateSign: Base Case: if array.length = 0. return []. Recursion: make the first item in array positive and the second negative. return [array[0], array[1]].concat(alternateSign(array.slice(2)));

--- src/recursion.js
+++ src/recursion.js
@@ -556,6 +556,18 @@ var minimizeZeroes = function(array) {
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
 var alternateSign = function(array) {
+
+  if (array.length === 0) {
+    return [];
+  }
+  if (array[0] < 0) {
+    array[0] *= -1;
+  }
+  if (array[1] > 0) {
+    array[1] *= -1;
+  }
+  return [array[0], array[1]].concat(alternateSign(array.slice(2)));
+
 };
 
 // 36. Given a string, return a string with digits converted to their word equivalent.
